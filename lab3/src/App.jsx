4eaca6902983cc2f4d6955ea3ec92fd8e5c731e3
FantaSnow <55027797+FantaSnow@users.noreply.github.com>
2024-09-19 22:48:07 +0300

    lab3

---
+++ lab3/src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./App.css";
+import ToDoTable from "./components/ToDoTable";
+import AddToDoComponent from "./components/AddToDoComponent";
+import SearchInput from "./components/SearchInput";
+import PageTitle from "./components/PageTitle";
+
+function App() {
+  const [toDos, setToDos] = useState([]);
+  const [newToDo, setNewToDo] = useState({ title: "" });
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleNewTitleChange(event) {
+    setNewToDo({ id: Math.random(), title: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (newToDo.title.trim()) {
+      setToDos([...toDos, newToDo]);
+      setNewToDo({ title: "" });
+    }
+  }
+
+  function handleDelete(id) {
+    const updatedToDos = toDos.filter((toDo) => toDo.id !== id);
+    setToDos(updatedToDos);
+  }
+
+  function handleSearchChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  const filteredToDos = toDos.filter((toDo) =>
+    toDo.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <PageTitle title="My ToDo App" />
+      <AddToDoComponent
+        title={newToDo.title}
+        onTitleChange={handleNewTitleChange}
+        onSubmit={handleSubmit}
+      />
+      <SearchInput
+        searchTerm={searchTerm}
+        onSearchChange={handleSearchChange}
+      />
+      <ToDoTable toDos={filteredToDos} onDelete={handleDelete} />
+    </div>
+  );
+}
+
+export default App;
