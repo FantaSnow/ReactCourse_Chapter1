1de3b303b1b83a4b1447de35d069f7ebe84587a5
FantaSnow <55027797+FantaSnow@users.noreply.github.com>
2024-09-29 17:04:32 +0300

    lab6

---
+++ lab6/src/components/AddToDoComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AddToDoComponent = ({ title = '', onTitleChange, onSubmit }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={title}
+        onChange={onTitleChange}
+        placeholder="Add new ToDo"
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default AddToDoComponent;
---
+++ lab6/src/components/SearchInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchInput = ({ searchTerm, onSearchChange }) => {
+  return (
+    <input
+      type="text"
+      value={searchTerm}
+      onChange={onSearchChange}
+      placeholder="Search ToDo"
+    />
+  );
+};
+
+export default SearchInput;
